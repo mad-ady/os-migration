8430450e8be6705b9bf6c1800bc22273656dca29
Adrian Popa <adrian.popa.gh@gmail.com>
2019-12-10 14:20:29 +0200

    Typo + more checks

--- user-accounts-source.sh
+++ user-accounts-source.sh
@@ -9,13 +9,18 @@ fi
 
 if [ "$#" -eq "1" ]; then
     DST=`mktemp -d -p "$1"`;
+    if [ -z "$DST" ]; then
+       echo "Unable to create destination temporary directory"
+       exit;
+    fi
+
     echo "*** Backing up user accounts to $DST ***"
     export UGIDLIMIT=500
     awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > "$DST/passwd.mig"
     awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > "$DST/group.mig"
     awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - |egrep -f - /etc/shadow > "$DST/shadow.mig"
     cp /etc/gshadow "$DST/gshadow.mig"
-    tar -zcvpf "$DST/home.tar.gz /home
+    tar -zcvpf "$DST/home.tar.gz" /home
 
     echo "*** Export done to $DST ***"
 else
